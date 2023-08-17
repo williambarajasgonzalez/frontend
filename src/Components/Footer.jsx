e6a83c717fd31cc3810c96fb385b3d89eaa77228
williambarajasgonzalez <127361009+williambarajasgonzalez@users.noreply.github.com>
2023-08-16 22:19:18 -0700

    Update Footer.jsx
    f

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,7 @@ import axios from "axios"
 import React from 'react';
 import ReactLoading from 'react-loading';
 import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
 
 
 export default function Footer(){
